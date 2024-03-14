659efe048f9bcded911c7b367e158b40ee54bdea
Ashish Bhosle <asbhosle2003@gmail.com>
2024-03-14 13:15:29 -0700

    new commit

--- binarySearch/1dArrays.cpp
+++ binarySearch/1dArrays.cpp
@@ -219,3 +219,199 @@ pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
     int last = lastOccurrence(arr, n, k);
     return {first, last};
 }
+
+// Number of Occurence
+
+int firstOccurrence(vector<int> &arr, int n, int k)
+{
+    int low = 0, high = n - 1;
+    int first = -1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k)
+        {
+            first = mid;
+            // look for smaller index on the left
+            high = mid - 1;
+        }
+        else if (arr[mid] < k)
+        {
+            low = mid + 1; // look on the right
+        }
+        else
+        {
+            high = mid - 1; // look on the left
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &arr, int n, int k)
+{
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k)
+        {
+            last = mid;
+            // look for larger index on the right
+            low = mid + 1;
+        }
+        else if (arr[mid] < k)
+        {
+            low = mid + 1; // look on the right
+        }
+        else
+        {
+            high = mid - 1; // look on the left
+        }
+    }
+    return last;
+}
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+{
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1)
+        return {-1, -1};
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
+
+int count(vector<int> &arr, int n, int x)
+{
+    // Write Your Code Here
+    pair<int, int> ans = firstAndLastPosition(arr, n, x);
+    if (ans.first == -1)
+        return 0;
+    return (ans.second - ans.first + 1);
+}
+
+//  Search In Rotated Sorted Array
+
+int search(vector<int> &arr, int n, int k)
+{
+    // Write your code here.
+    // Return the position of K in ARR else return -1.
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+
+        int mid = (low + high) / 2;
+        if (arr[mid] == k)
+        {
+            return mid;
+        }
+        // either right or left half is sorted
+        // if left one
+        if (arr[low] <= arr[mid])
+        {
+
+            // mid aur low k inside m h k
+            if (arr[low] <= k && k <= arr[mid])
+            {
+
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        { // right half is sorted
+
+            if (arr[mid] <= k && arr[high] >= k)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+//  Search In A Rotated Sorted Array II
+
+bool searchInARotatedSortedArrayII(vector<int> &arr, int k)
+{
+    // Write your code here.
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (high + low) / 2;
+        if (arr[mid] == k)
+            return true;
+
+        if (arr[low] == arr[mid] && arr[mid] == arr[high])
+        {
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
+
+        if (arr[low] <= arr[mid])
+        {
+            if (arr[low] <= k && k <= arr[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (arr[mid] <= k && k <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}
+
+// Find Minimum in Rotated Sorted Array
+
+int findMin(vector<int> &arr)
+{
+    // Write your code here.
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[low] <= arr[mid])
+        {
+            ans = min(ans, arr[low]);
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+            ans = min(ans, arr[mid]);
+        }
+    }
+    return ans;
+}
